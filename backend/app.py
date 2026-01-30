fcabc502c245ed2f149c5192404f6d93de0a2571
Ashish Lahire <ashishlahire12@gmail.com>
2026-01-30 16:57:56 +0530

    made commits in app.py

--- backend/app.py
+++ backend/app.py
@@ -157,7 +157,7 @@ if __name__ == '__main__':
     
      # Use PORT from environment (Render sets this)
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run()
     
     # python -m http.server 8000
     # python backend/app.py
